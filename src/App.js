755273d5b648ea9376188d85a81b30533be25a4f
Csaba Balint <tsaba99@gmail.com>
2022-03-09 23:57:30 +0000

    Create layout and add styling

--- src/App.js
+++ src/App.js
@@ -2,24 +2,23 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import DateAndTime from "./Components/DateAndTime";
 import SearchInput from './Components/SearchInput';
+import { faBoltLightning, faCloud, faCloudRain, faCloudShowersHeavy, faCloudSun, faCloudSunRain, faSnowflake, faSun, faTemperatureEmpty, faTemperatureHalf, faTemperatureQuarter, faTemperatureLow, faTemperatureThreeQuarters, faUmbrella, faWind, faTemperatureHigh, faTemperatureFull, } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   const [query, setQuery] = useState('');
   const [weather, setWeather] = useState({});
-  const [forecast, setForecast] = useState([]);
-
 
   const api = {
     key: "9f12bf41d59e05b86cd8d5c90f24a139",
     base: "http://api.openweathermap.org/data/2.5/"
   }
 
-
-
   const search = e => {
     if (e.key === "Enter") {
       axios.get(`${api.base}weather?q=${query}&units=metric&appid=${api.key}`)
         .then(weatherData => {
+          console.log(weatherData.data);
           setWeather(weatherData.data);
           console.log(weatherData.data);
           setQuery('');
@@ -30,63 +29,62 @@ function App() {
     }
   }
 
-  const getForecast = () => {
-    axios.get(`${api.base}forecast?lat=${weather.coord.lon}&lon=${weather.coord.lon}&units=metric&appid=${api.key}`)
-      .then(forecast => {
-        setForecast(forecast.data);
-        console.log(forecast.data)
-      })
+  const svgs = <div>
+    <FontAwesomeIcon icon={faSun} />
+    <FontAwesomeIcon icon={faCloud} />
+    <FontAwesomeIcon icon={faUmbrella} />
+    <FontAwesomeIcon icon={faSnowflake} />
+    <FontAwesomeIcon icon={faWind} />
+    <FontAwesomeIcon icon={faCloudRain} />
+    <FontAwesomeIcon icon={faCloudSunRain} />
+    <FontAwesomeIcon icon={faCloudSun} />
+    <FontAwesomeIcon icon={faCloudShowersHeavy} />
+    <FontAwesomeIcon icon={faBoltLightning} />
+    <FontAwesomeIcon icon={faTemperatureEmpty} />
+    <FontAwesomeIcon icon={faTemperatureLow} />
+    <FontAwesomeIcon icon={faTemperatureQuarter} />
+    <FontAwesomeIcon icon={faTemperatureHalf} />
+    <FontAwesomeIcon icon={faTemperatureThreeQuarters} />
+    <FontAwesomeIcon icon={faTemperatureHigh} />
+    <FontAwesomeIcon icon={faTemperatureFull} />
+    <FontAwesomeIcon icon={faTemperatureFull} />
+    <FontAwesomeIcon icon={faTemperatureFull} />
+
+  </div>;
+
+  const convertToHoursAndMins = unixTimestamp => {
+    const date = new Date(unixTimestamp * 1000);
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    if (minutes < 10) {
+      return `${hours}:0${minutes}`
+    }
+    return `${hours}:${minutes}`
   }
-
-
-
-  let tempsArray = [];
-  if (Object.keys(forecast).length) {
-    tempsArray = forecast.list.map((listItem, index) => {
-      return (
-        <React.Fragment key={index++}>
-          <ul className='forecast-item'>
-            <li>{listItem.main.temp}°</li>
-            <li>{listItem.weather[0].main}</li>
-            <li>Humidity : {listItem.main.humidity}%</li>
-            <li>{listItem.dt_txt.slice(0, -3)}</li>
-          </ul>
-        </React.Fragment>
-      )
-    })
-  }
-
 
   return (
-    <div className="App">
-      <main>
-        <div className="search-box">
-          <SearchInput search={search} setQuery={setQuery} query={query} />
-        </div>
-        {(typeof weather.main !== "undefined") ? (
-          <div>
-            <div className="location-box">
-              <div className="location">{weather.name} {weather.sys.country}</div>
-              <DateAndTime />
-            </div>
-            <div className="weahter=box">
-              <div className="temp">
-                {Math.round(weather.main.temp)}°
-              </div>
-              <div className="weather">{weather.weather[0].main}</div>
-            </div>
-            <button
-              className="getForecast-btn"
-              onClick={getForecast}
-            >
-              Get forecast
-            </button>
-            <div className='forecast-box'>
-              {tempsArray.length > 0 ? tempsArray : ('')}
-            </div>
+    <div className="app">
+      <div className="search-box">
+        <SearchInput search={search} setQuery={setQuery} query={query} />
+      </div>
+      {(typeof weather.main !== "undefined") ? (
+        <div >
+          <div className="location">
+            {weather.name}, {weather.sys.country}
           </div>
-        ) : ('')}
-      </main>
+          <div className="temp">
+            {Math.round(weather.main.temp)}°
+          </div>
+          <div className="weather">
+            {weather.weather[0].description}
+          </div>
+          <DateAndTime />
+          <ul className="sunrise-sunset-box">
+            <li className="sunrise">{convertToHoursAndMins(weather.sys.sunrise)}</li>
+            <li className="sunset">{convertToHoursAndMins(weather.sys.sunset)}</li>
+          </ul>
+        </div>
+      ) : ('')}
     </div>
   )
 }
--- src/Components/DateAndTime.js
+++ src/Components/DateAndTime.js
@@ -2,7 +2,7 @@ export default function DateAndTime() {
 
     const dateBuilder = (d) => {
         let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-        let months =  ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
         let day = days[d.getDay()];
         let date = d.getDate();
@@ -10,8 +10,12 @@ export default function DateAndTime() {
         let year = d.getFullYear();
         let hour = d.getHours();
         let minutes = d.getMinutes();
+        if (minutes < 10) {
+            return (`${day}, ${date} ${month} ${year}, ${hour}:0${minutes}`)
+        } else {
+            return (`${day}, ${date} ${month} ${year}, ${hour}:${minutes}`)
+        }
 
-        return (`${day}, ${date} ${month} ${year}, ${hour}:${minutes}`)
     }
 
     return (
